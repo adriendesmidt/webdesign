7a50d144f1e54dc0d2652d6f02769991c1bed31d
remiguyen <roumixg@gmail.com>
2017-02-24 15:41:45 +0100

    ajout: - bibli angular & archive JsApp dans le dossier script - App dans le body de l'html - directive ng-repeat d'essai après le tableau

---
+++ app/style/js/JSapp.js
@@ -0,0 +1,35 @@
+var varModule = angular.module('MyModule',[]);
+
+varModule.controller('controleDonnees', function($scope){
+	
+	$scope.donnees = [
+						{
+						"username":"julien",
+						"age":"15"
+						},
+						{
+						"username":"rémi",
+						"age":"21"
+						},
+						{
+						"username":"Pierre",
+						"age":"indéterminé"
+						},
+						{
+						"username":"Indiana",
+						"age":"osef"
+						},
+						{
+						"username":"Boulais",
+						"age":"23"
+						},
+						{
+						"username":"Adrien",
+						"age":"22"
+						}
+
+					
+					
+					];
+				}
+			)
