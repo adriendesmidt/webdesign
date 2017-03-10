3e32fc31aae491e6063bd060f95e8a4de1b8486f
remiguyen <roumixg@gmail.com>
2017-03-10 09:28:01 +0100

    essai avec bibli json

--- app/style/js/JSapp.js
+++ app/style/js/JSapp.js
@@ -2,34 +2,40 @@ var varModule = angular.module('MyModule',[]);
 
 varModule.controller('controleDonnees', function($scope){
 	
-	$scope.donnees = [
-						{
-						"username":"julien",
-						"age":"15"
-						},
-						{
-						"username":"rémi",
-						"age":"21"
-						},
-						{
-						"username":"Pierre",
-						"age":"indéterminé"
-						},
-						{
-						"username":"Indiana",
-						"age":"osef"
-						},
-						{
-						"username":"Boulais",
-						"age":"23"
-						},
-						{
-						"username":"Adrien",
-						"age":"22"
-						}
+	// $scope.donnees = [
+						// {
+						// "username":"julien",
+						// "age":"15"
+						// },
+						// {
+						// "username":"rémi",
+						// "age":"21"
+						// },
+						// {
+						// "username":"Pierre",
+						// "age":"indéterminé"
+						// },
+						// {
+						// "username":"Indiana",
+						// "age":"osef"
+						// },
+						// {
+						// "username":"Boulais",
+						// "age":"23"
+						// },
+						// {
+						// "username":"Adrien",
+						// "age":"22"
+						// }
 
 					
 					
-					];
+					// ];
+					
+					$http.get('parcs-de-stationnement-concedes-de-la-ville-de-paris.json').then(function(response){
+			$scope.donnees = response.data.records;
+			
+		});	
+					
 				}
-			)
+			);
